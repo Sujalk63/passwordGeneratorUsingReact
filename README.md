71d2d667449f0564adbcd73724fe5a5e93bc3a0a
Sujal Karmakar <sujalkarmakar6363@gmail.com>
2024-02-18 13:41:29 +0530

    added all files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Password Generator using React
+
+This is a simple password generator application built with React. It allows users to generate passwords with customizable length and character options.
+
+## Features
+
+- Demonstrated usage of React hooks including `useCallback`, `useEffect`, and `useRef`.
+- Utilized Tailwind CSS for styling and layout.
+
+## Technologies Used
+
+- React
+- Tailwind CSS
+- JavaScript
---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+// import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // useRef hook, to copt the password
+
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numAllowed) {
+      str += "0123456789";
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*_+";
+    }
+
+    for (let i = 1; i <=length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+
+  }, [length, numAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToClipBoard = useCallback(()=> {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {passwordGenerator()}, [length, numAllowed, charAllowed, passwordGenerator] )
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700 ">
+        <h1 className="text-white text-center ">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button onClick={copyPasswordToClipBoard} className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1 ">
+            <input
+              type="range"
+              min={8}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label htmlFor="">length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numAllowed}
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="">Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+
+/*
+  Summary:
+  - The useCallback hook is used to memoize the passwordGenerator function, ensuring that it only changes when necessary to avoid unnecessary re-renders caused by the creation of new function instances.
+  - Since the passwordGenerator function depends only on the setPassword function to update the password state, setPassword is the only dependency needed in the useCallback dependency array.
+  - Including numAllowed, length, or charAllowed as dependencies in useCallback is unnecessary, as these values do not directly affect the behavior of the passwordGenerator function.
+  - By memoizing the passwordGenerator function with useCallback and including only setPassword as a dependency, we optimize performance by preventing unnecessary recreations of the function instance when its dependencies haven't changed.
+*/
+
